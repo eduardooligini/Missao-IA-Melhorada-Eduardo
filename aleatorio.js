fce91ec11449092bee0f24ada153709f757e384a
eduardooligini <145785071+eduardooligini@users.noreply.github.com>
2025-11-25 12:13:04 -0300

    26

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,10 @@
+const nomes = ["Tamiris","Laura","Eduardo","Nathieli","Lauana","Denise","Emer"];
+
+
+
 export function aleatorio(lista){
 const posicao = Math.floor(Math.random()*lista.length);
 return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
